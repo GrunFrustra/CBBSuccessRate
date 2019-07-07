c7de6cf99565021ddd71782715eba61bcd93b28a
Ryan Geary <ryang1991@gmail.com>
2019-07-07 17:26:37 -0400

    Data retrieval added
    Extracted data needed to calculate success rate for each team.

--- parser.py
+++ parser.py
@@ -9,16 +9,46 @@ from urllib.request import urlopen
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import re
+import teamStats
 
-
+#URL leads to game data at sports-reference
 gamePage = 'https://www.sports-reference.com/cbb/boxscores/2019-04-08-21-virginia.html'
-#class="section_anchor" data-label="Texas Tech (31-7)" 
-#id="box-score-basic-texas-tech_link"></span>, 
 
-#bs.find('div', {'id':'bodyContent'}).find_all('a',
-#href=re.compile('^(/wiki/)((?!:).)*$'))
+
+#Strip tag data away to get team names
+def getTeamNames(bs):
+    labels = bs.find_all('span',{'data-label':re.compile('\)$')})
+    teams = []
+    for x in labels:
+        team_with_record = x['data-label']
+        team_name = re.sub('\(.*\)','', team_with_record)
+        team_name = team_name.strip()
+        teams.append(team_name)
+    return teams
+
+#Use BeautifulSoup to get table data, then call appropriate functions
+def getTeamData(bs, t1, t2):
+    labels = bs.find_all('tfoot')
+    print(labels[0])
+    print(labels[1])
+    t1.fillValues(labels[0])
+    t2.fillValues(labels[1])
+
+
 html = urlopen(gamePage)
 bs = BeautifulSoup(html, 'html.parser')
-labels = bs.find_all('span',{'data-label':re.compile('\)$')})
-for x in labels:
-    print(x)
+
+teams = getTeamNames(bs)
+print(teams)
+
+team1_data = teamStats.Stats(teams[0])
+team2_data = teamStats.Stats(teams[1])
+getTeamData(bs, team1_data, team2_data)
+
+
+print(team1_data.name)
+print(team1_data.field_goals_made)
+print(team1_data.field_goals_attempted)
+print(team1_data.fouls)
+print(team1_data.steals)
+print(team1_data.turnovers)
---
+++ teamStats.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  7 16:24:24 2019
+
+@author: ryang
+"""
+
+class Stats():
+    def __init__(self, tname):
+        self.name = tname
+        self.field_goals_made = 0
+        self.field_goals_attempted = 0
+        self.steals = 0
+        self.turnovers = 0
+        self.fouls = 0
+        
+    #Fill Stats with categories needed to calculate Success Rate
+    def fillValues(self, statString):
+        self.field_goals_made = statString.find('td',{'data-stat':'fg'}).text
+        self.field_goals_attempted = statString.find('td',{'data-stat':'fga'}).text
+        self.steals = statString.find('td',{'data-stat':'stl'}).text
+        self.turnovers = statString.find('td',{'data-stat':'tov'}).text
+        self.fouls = statString.find('td',{'data-stat':'pf'}).text
